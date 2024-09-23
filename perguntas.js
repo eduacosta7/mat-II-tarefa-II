483dfa53ef4352f11842031c72588c8aeffbd6c5
edu <131039327+eduacosta7@users.noreply.github.com>
2024-09-23 08:02:02 -0300

    aa

---
+++ perguntas.js
@@ -0,0 +1,62 @@
+const caixaPrincipal = document.querySelector('caixa-principal')
+const caixaPerguntas = document.querySelector('caixa-perguntas')
+const caixaAlternativas = document.querySelector('caixa-altrnativa')
+const caixaResultado = document.querySelector('caixa-resultado')
+const textoResultado = document.querySelector('texto-resultado')
+
+const perguntas = [
+    {
+        enunciado: "No âmbito social, você prefere",
+        alternativas: [
+            {
+                texto: "Alternativa1",
+                afirmação: "afirmação",
+            },
+
+            {
+                texto: "Alternativa2",
+                afirmação: "afirmação",
+            }]
+    },
+
+    {
+        enunciado: "No âmbito ambiental, você prefere", alternativas: [
+            { texto: "Alternativa3", afirmação: "afirmação", },
+            { texto: "Alternativa4", afirmação: "afirmação", }]
+    },
+
+    {
+        enunciado: "No âmbito tecnológico, você prefere", alternativas: [
+            { texto: "Alternativa5", afirmação: "afirmação", },
+            { texto: "Alternativa6", afirmação: "afirmação", }]
+    }
+
+]
+
+
+
+let atual = 0;
+let perguntaAtual;
+function mostraPerguntas() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "."
+    mostraAlternativas();
+}
+mostraAlternativas();
+for (const alternativas of perguntaAtual.alternativas) {
+    const botaoAlternativa = documnt.createElement("button")
+    botaoAlternativa.textContent = alternativa.texto;
+    botaoAlternativa.addeventlitenr("click", () => respostaSelecionada(pergunta));
+    caixaAlternativas.appendChild(botãoAlternativa)
+}
+
+function respostaSelecionada(pergunta) {
+    const afirmações = pergunta.afirmação
+    atual++;
+    mostraPerguntas();
+
+}
+
+function mostraRsultado()
+    
--- script.js
+++ script.js
@@ -1,63 +1,31 @@
-const caixaPrincipal = document.querySelector('caixa-principal')
-const caixaPerguntas = document.querySelector('caixa-perguntas')
-const caixaAlternativas = document.querySelector('caixa-altrnativa')
-const caixaResultado = document.querySelector('caixa-resultado')
-const textoResultado = document.querySelector('texto-resultado')
-
-const perguntas = [
-    {
-        enunciado: "No âmbito social, você prefere",
-        alternativas: [
-            {
-                texto: "Alternativa1",
-                afirmação: "afirmação",
-            },
-
-            {
-                texto: "Alternativa2",
-                afirmação: "afirmação",
-            }]
-    },
-
-    {
-        enunciado: "No âmbito ambiental, você prefere", alternativas: [
-            { texto: "Alternativa3", afirmação: "afirmação", },
-            { texto: "Alternativa4", afirmação: "afirmação", }]
-    },
-
-    {
-        enunciado: "No âmbito tecnológico, você prefere", alternativas: [
-            { texto: "Alternativa5", afirmação: "afirmação", },
-            { texto: "Alternativa6", afirmação: "afirmação", }]
-    }
-
-]
-
-
-
-let atual = 0;
-let perguntaAtual;
-function mostraPerguntas() {
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = "."
-    mostraAlternativas();
+function criaCartao(categoria, pergunta, resposta){
 }
-mostraAlternativas();
-for (const alternativas of perguntaAtual.alternativas) {
-    const botaoAlternativa = documnt.createElement("button")
-    botaoAlternativa.textContent = alternativa.texto;
-    botaoAlternativa.addeventlitenr("click", () => respostaSelecionada(pergunta));
-    caixaAlternativas.appendChild(botãoAlternativa)
-}
+let container =
+document.getElementById('container');
+let cartao = document.createElement('article');
+cartao.className= 'cartao';
+cartao.innerHTML =
+`
 
-function respostaSelecionada(pergunta) {
-    const afirmações = pergunta.afirmação
-    atual++;
-    mostraPerguntas();
-
-}
-
-function mostraRsultado()
+<div class="conteudo-cartao">
+<h3>${categoria}</h3>
+<div class="pergunta-cartao">
+<p>${pergunta}</p>
+</div>
+<div class="resposta-cartao">
+    <p>${resposta}</p>
     
+</div>
+</div>
+`
+container.appendChild(cartao);
+
 }
+
+
+
+
+
+
+
+
