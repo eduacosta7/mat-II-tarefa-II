7940b28cfd2ab7edce43e6e80f7943d968f7a1ee
edu <131039327+eduacosta7@users.noreply.github.com>
2024-10-07 08:02:53 -0300

    aa

--- perguntas.js
+++ perguntas.js
@@ -1,121 +1,121 @@
 criaCartao(
-    'História'
-'Quem descobriu o Brasil?'
-'Pedro Alvárez Cabral'
+    'História',
+'Quem descobriu o Brasil?',
+'Pedro Alvárez Cabral',
 )
 
 criaCartao(
- 'História'
- 'Quem criou o Anglicanismo?'
-'Henrique VIII'
+ 'História',
+ 'Quem criou o Anglicanismo?',
+'Henrique VIII',
 )
 
 criaCartao(
-    'História'
- 'Quem foi o primeiro presidente eleito por voto popular no Brasil?'
- 'Prudente Morais'
+    'História',
+ 'Quem foi o primeiro presidente eleito por voto popular no Brasil?',
+ 'Prudente Morais',
 )
 
 criaCartao(
-    'História'
- 'Em que ano acabou a Segunda Guerra Mundial'
-  '1945'
+    'História',
+ 'Em que ano acabou a Segunda Guerra Mundial',
+  '1945',
 )
 
 criaCartao(
-    'História'
-   'Em que ano foi decretado o Ato Inconstitucional 5 no Brasil?'
-  'Em 1968'
+    'História',
+   'Em que ano foi decretado o Ato Inconstitucional 5 no Brasil?',
+  'Em 1968',
 )
 
 criaCartao(
-    'História'
-   'Qual foi Primeiro país da america a abolir a escravidão?'
-    'O Haiti'
+    'História',
+   'Qual foi Primeiro país da america a abolir a escravidão?',
+    'O Haiti',
 )
 
 criaCartao(
-    'História'
-    'Em que ano derrubaram o muro de Berlim'
-    '1989'
+    'História',
+    'Em que ano derrubaram o muro de Berlim',
+    '1989',
 )
 
 criaCartao(
-    'História'
-   'Em que ano acabou a URSS'
-   '1991'
+    'História',
+   'Em que ano acabou a URSS',
+   '1991',
 )
 
 criaCartao(
-    'História'
-      'Qual o país mais novo do mundo(até 2023)?'
-    'Sudão do Sul'
+    'História',
+      'Qual o país mais novo do mundo(até 2023)?',
+    'Sudão do Sul',
 )
 
 criaCartao(
-    'História'
-    'Quantas cores tem na bandeira da França'
-   '3'
+    'História',
+    'Quantas cores tem na bandeira da França',
+   '3',
 )
 
 criaCartao(
-    'Geográfia'
-    'Quantos biomas tem no Brasil'
-     '6'
+    'Geográfia',
+    'Quantos biomas tem no Brasil',
+     '6',
 )
 
 criaCartao(
-    'Geográfia'
-     'Qual o bioma predominante da região sul do Brasil'
-     'A mata Atlântica'
+    'Geográfia',
+     'Qual o bioma predominante da região sul do Brasil',
+     'A mata Atlântica',
 )
 
 criaCartao(
-    'Geográfia'
-     'Qual o bioma predominante da região nordeste do Brasil'
-     'A caatinga'
+    'Geográfia',
+     'Qual o bioma predominante da região nordeste do Brasil',
+     'A caatinga',
 )
 
 criaCartao(
-    'Geográfia'
-    'Qual a capital da Grécia?'
-    'Atenas'
+    'Geográfia',
+    'Qual a capital da Grécia?',
+    'Atenas',
 )
 
 criaCartao(
-    'Geográfia'
-     'Qual a capital do Paraná'
-     'Curitiba'
+    'Geográfia',
+     'Qual a capital do Paraná',
+     'Curitiba',
 )
 
 criaCartao(
-    'Geográfia'
-     'Qual a maior tribo indígena do Brasil?'
-    'Guarani'
+    'Geográfia',
+     'Qual a maior tribo indígena do Brasil?',
+    'Guarani',
 )
 
 criaCartao(
-    'Geográfia'
-     'Quantas luas tem o planeta Jupíter?'
-    '92'
+    'Geográfia',
+     'Quantas luas tem o planeta Jupíter?',
+    '92',
 )
 
 criaCartao(
-    'Geográfia'
-     'Quanto planetas tem no Sistema Solar?'
-    '8'
+    'Geográfia',
+     'Quanto planetas tem no Sistema Solar?',
+    '8',
 )
 
 criaCartao(
-    'Geográfia'
-     'São Paulo é uma Megalópole?'
-    'Não'
+    'Geográfia',
+     'São Paulo é uma Megalópole?',
+    'Não',
 )
 
 criaCartao(
-    'Geográfia'
-      'São Paulo é uma Mega Cidade?'
-    'Sim'
+    'Geográfia',
+      'São Paulo é uma Mega Cidade?',
+    'Sim',
 )
 
 
