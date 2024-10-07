4e9f44fc381ddcbe9c6a44850509a5ccf62ef2f2
edu <131039327+eduacosta7@users.noreply.github.com>
2024-10-07 07:52:55 -0300

    aa

--- main.js
+++ main.js
@@ -21,11 +21,3 @@ cartao.innerHTML =
 container.appendChild(cartao);
 
 }
-
-
-
-
-
-
-
-
--- perguntas.js
+++ perguntas.js
@@ -1,62 +1,121 @@
-const caixaPrincipal = document.querySelector('caixa-principal')
-const caixaPerguntas = document.querySelector('caixa-perguntas')
-const caixaAlternativas = document.querySelector('caixa-altrnativa')
-const caixaResultado = document.querySelector('caixa-resultado')
-const textoResultado = document.querySelector('texto-resultado')
+criaCartao(
+    'História'
+'Quem descobriu o Brasil?'
+'Pedro Alvárez Cabral'
+)
 
-const perguntas = [
-    {
-        enunciado: "No âmbito social, você prefere",
-        alternativas: [
-            {
-                texto: "Alternativa1",
-                afirmação: "afirmação",
-            },
+criaCartao(
+ 'História'
+ 'Quem criou o Anglicanismo?'
+'Henrique VIII'
+)
 
-            {
-                texto: "Alternativa2",
-                afirmação: "afirmação",
-            }]
-    },
+criaCartao(
+    'História'
+ 'Quem foi o primeiro presidente eleito por voto popular no Brasil?'
+ 'Prudente Morais'
+)
 
-    {
-        enunciado: "No âmbito ambiental, você prefere", alternativas: [
-            { texto: "Alternativa3", afirmação: "afirmação", },
-            { texto: "Alternativa4", afirmação: "afirmação", }]
-    },
+criaCartao(
+    'História'
+ 'Em que ano acabou a Segunda Guerra Mundial'
+  '1945'
+)
 
-    {
-        enunciado: "No âmbito tecnológico, você prefere", alternativas: [
-            { texto: "Alternativa5", afirmação: "afirmação", },
-            { texto: "Alternativa6", afirmação: "afirmação", }]
-    }
+criaCartao(
+    'História'
+   'Em que ano foi decretado o Ato Inconstitucional 5 no Brasil?'
+  'Em 1968'
+)
 
-]
+criaCartao(
+    'História'
+   'Qual foi Primeiro país da america a abolir a escravidão?'
+    'O Haiti'
+)
+
+criaCartao(
+    'História'
+    'Em que ano derrubaram o muro de Berlim'
+    '1989'
+)
+
+criaCartao(
+    'História'
+   'Em que ano acabou a URSS'
+   '1991'
+)
+
+criaCartao(
+    'História'
+      'Qual o país mais novo do mundo(até 2023)?'
+    'Sudão do Sul'
+)
+
+criaCartao(
+    'História'
+    'Quantas cores tem na bandeira da França'
+   '3'
+)
+
+criaCartao(
+    'Geográfia'
+    'Quantos biomas tem no Brasil'
+     '6'
+)
+
+criaCartao(
+    'Geográfia'
+     'Qual o bioma predominante da região sul do Brasil'
+     'A mata Atlântica'
+)
+
+criaCartao(
+    'Geográfia'
+     'Qual o bioma predominante da região nordeste do Brasil'
+     'A caatinga'
+)
+
+criaCartao(
+    'Geográfia'
+    'Qual a capital da Grécia?'
+    'Atenas'
+)
+
+criaCartao(
+    'Geográfia'
+     'Qual a capital do Paraná'
+     'Curitiba'
+)
+
+criaCartao(
+    'Geográfia'
+     'Qual a maior tribo indígena do Brasil?'
+    'Guarani'
+)
+
+criaCartao(
+    'Geográfia'
+     'Quantas luas tem o planeta Jupíter?'
+    '92'
+)
+
+criaCartao(
+    'Geográfia'
+     'Quanto planetas tem no Sistema Solar?'
+    '8'
+)
+
+criaCartao(
+    'Geográfia'
+     'São Paulo é uma Megalópole?'
+    'Não'
+)
+
+criaCartao(
+    'Geográfia'
+      'São Paulo é uma Mega Cidade?'
+    'Sim'
+)
 
 
-
-let atual = 0;
-let perguntaAtual;
-function mostraPerguntas() {
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = "."
-    mostraAlternativas();
-}
-mostraAlternativas();
-for (const alternativas of perguntaAtual.alternativas) {
-    const botaoAlternativa = documnt.createElement("button")
-    botaoAlternativa.textContent = alternativa.texto;
-    botaoAlternativa.addeventlitenr("click", () => respostaSelecionada(pergunta));
-    caixaAlternativas.appendChild(botãoAlternativa)
-}
-
-function respostaSelecionada(pergunta) {
-    const afirmações = pergunta.afirmação
-    atual++;
-    mostraPerguntas();
-
-}
-
-function mostraRsultado()
-    
